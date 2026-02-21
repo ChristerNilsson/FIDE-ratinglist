95c582808c45d31cb7829032b5e2072a28ade87a
Christer Nilsson <janchrister.nilsson@gmail.com>
2026-02-21 16:10:28 +0100

    Tog bort 0 0 0

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 Givet FIDE-id hämtar programmet namn och elo-rating för alla spelare i hela världen, 1.7M  
 Filen hämtas från FIDE och är cirka 273 MB stor. 
-* Hämta den stora databasen från FIDE och se till att filen heter **fide.txt**
+* Hämta den stora databasen från FIDE
   * http://ratings.fide.com/download/players_list.zip
+  * Observera att https inte fungerar
+  * Observera att filen förväntas heta **players_list_foa.txt**
   * Detta ska ske den andra i varje månad
 * Därefter körs pythonprogrammet **fide.py** som skapar 1000 json-filer i katalogen **shards**.
+* Kopiera katalogen shards till bl a 2025/035-FairPair
 * När detta är gjort kan man via shards hämta en person.
   * Man skriver in ett FIDE-id och då hämtas personens namn och elo till en listbox
   * FIDE-id behövs för att kunna skapa en TRF16-fil.
--- fide.py
+++ fide.py
@@ -4,28 +4,26 @@ import time
 hash = {}
 start = time.time()
 
-for i in range(1000):
-	shard = str(i)
-	while len(shard) < 3: shard = "0" + shard
-	hash[shard] = {}
-
 def fetchFIDE():
-	with open('fide.txt', 'r', encoding='utf-8') as f:
+	with open('players_list_foa.txt', 'r', encoding='utf-8') as f:
 		lines = f.readlines()
 		for line in lines[1:]:
 			fideId = line[0:15].strip()
 			name = line[15:75].strip()
+
 			classic = int(line[113:117].strip())
 			rapid = int(line[126:130].strip())
 			blitz = int(line[139:143].strip())
 
-			# shard = fideId[-4:-1]
+			if classic == 0 and rapid == 0 and blitz == 0: continue
+
 			shard = fideId[0:3]
 
+			if shard not in hash: hash[shard] = {}
 			h = hash[shard]
 			h[fideId] = [classic,rapid,blitz,name]
-	for i in range(1000):
-		shard = str(i)
+	for key in hash:
+		shard = key
 		while len(shard) < 3: shard = "0" + shard
 		with open('shards/' + shard + '.json', 'w', encoding='utf-8') as g:
 			json.dump(hash[shard], g, ensure_ascii=False)
